80d71b783d0d108ebf8b18f407793935f8da6a42
NOZA Jean-Pierre <nozajean-pierre@MacBookAir.mshome.net>
2025-02-12 22:32:59 -0300

    Ajout de la search bar

---
+++ src/app/pipe/search-artisans.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SearchArtisansPipe } from './search-artisans.pipe';
+
+describe('SearchArtisansPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchArtisansPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
